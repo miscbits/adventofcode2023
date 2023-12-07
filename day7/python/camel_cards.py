0959823e30abb0fbc8e76ce94a4a7bc3f251f1be
Wilhem Alcivar <alcivaw@wz.hasbro.com>
2023-12-07 14:59:20 -0800

    added more comprehensive tests and refactored quality counter

--- day7/python/camel_cards.py
+++ day7/python/camel_cards.py
@@ -1,13 +1,5 @@
+from __future__ import annotations
 from dataclasses import dataclass
-from dataclasses import field
-
-SHARED_VAL_MAP = {
-    '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, 'T': 9, 'J': 10, 'Q': 11, 'K': 12, 'A': 13,
-}
-
-SHARED_VAL_MAP_JOKERS = {
-    'J': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'Q': 11, 'K': 12, 'A': 13,
-}
 
 TIE_BREAK_MAP_JOKER = {
     'J': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f', '7': 'g', '8': 'h', '9': 'i', 'T': 'j', 'Q': 'k', 'K': 'l', 'A': 'm',
@@ -17,146 +9,58 @@ TIE_BREAK_MAP = {
     '2': 'a', '3': 'b', '4': 'c', '5': 'd', '6': 'e', '7': 'f', '8': 'g', '9': 'h', 'T': 'i', 'J': 'j', 'Q': 'k', 'K': 'l', 'A': 'm',
 }
 
-FIVE_OF_A_KIND = 10**15
-FOUR_OF_A_KIND = 10**14
-FULL_HOUSE = 10**13
-THREE_OF_A_KIND = 10**12
-TWO_PAIR = 10**11
-ONE_PAIR = 10**10
-
-TYPE_FIVE_OF_A_KIND = 6
-TYPE_FOUR_OF_A_KIND = 5
-TYPE_FULL_HOUSE = 4
-TYPE_THREE_OF_A_KIND = 3
-TYPE_TWO_PAIR = 2
-TYPE_ONE_PAIR = 1
+FIVE_OF_A_KIND = 6
+FOUR_OF_A_KIND = 5
+FULL_HOUSE = 4
+THREE_OF_A_KIND = 3
+TWO_PAIR = 2
+ONE_PAIR = 1
 
 @dataclass(order=True, kw_only=True)
 class Hand:
     hand_type: int = 0
     tie_break: str = ""
-    quality: int = 0
     cards: str
     bet: int
-    joker_count: int = 0
+    jacks_are_wild: bool = False
 
-    def calculate_quality(self):
-        self.quality = 0
-        card_counter = {}
-        for i, card in enumerate(self.cards):
-            self.quality += SHARED_VAL_MAP[card] * (10 ** ((len(self.cards) - i - 1) * 2)) # [0, 2, 4, 6, 8]
-            self.tie_break += TIE_BREAK_MAP[card]
-            card_counter[card] = card_counter.get(card, 0) + 1
-        card_types, card_vals = card_counter.keys(), card_counter.values()
-        # 5 of a kind
-        if len(card_types) == 1:
-            self.quality += FIVE_OF_A_KIND
-            self.hand_type=TYPE_FIVE_OF_A_KIND
-            return self
-        if len(card_types) == 2:
-            # 4 of a kind
-            if 4 in card_vals:
-                self.quality += FOUR_OF_A_KIND
-                self.hand_type=TYPE_FOUR_OF_A_KIND
-            # full house
-            elif 3 in card_vals:
-                self.quality += FULL_HOUSE
-                self.hand_type=TYPE_FULL_HOUSE
-                return self
-        # 3 of a kind. can't be a full house because previous check
-        if 3 in card_vals:
-            self.quality += THREE_OF_A_KIND
-            self.hand_type=TYPE_THREE_OF_A_KIND
-        #  two pair
-        elif list(card_vals).count(2) == 2:
-            self.quality += TWO_PAIR
-            self.hand_type=TYPE_TWO_PAIR
-        # one pair
-        elif 2 in card_vals:
-            self.quality += ONE_PAIR
-            self.hand_type=TYPE_ONE_PAIR
+    def calculate_tie_break(self) -> Hand:
+        self.tie_break = "".join([TIE_BREAK_MAP_JOKER[card] if self.jacks_are_wild else TIE_BREAK_MAP[card] for card in self.cards])
         return self
 
-    def calculate_quality_jokers(self):
-        self.quality = 0
+    def calculate_quality(self) -> Hand:
         card_counter = {}
-        for i, card in enumerate(self.cards):
-            self.quality += SHARED_VAL_MAP_JOKERS[card] * (10 ** ((len(self.cards) - i - 1) * 2)) # [0, 2, 4, 6, 8]
-            self.tie_break += TIE_BREAK_MAP_JOKER[card]
-            if card == 'J':
+        for card in self.cards:
+            if self.jacks_are_wild and card == 'J':
                 continue
             card_counter[card] = card_counter.get(card, 0) + 1
-        card_types, card_vals = card_counter.keys(), card_counter.values()
-        # one or zero card types is always five of a kind
-        # this covers all cases like [4], [3], [2], [1] so we wont have to check for that with any other combination of hands + jokers
-        # this also is always true if joker count is 4 or 5
-        if len(card_types) <= 1:
-            self.quality += FIVE_OF_A_KIND
-            self.hand_type = TYPE_FIVE_OF_A_KIND
-            return self
-        if self.joker_count <= 0:
-            if len(card_types) == 2:
-                # 4 of a kind
-                if 4 in card_vals:
-                    self.quality += FOUR_OF_A_KIND
-                    self.hand_type = TYPE_FOUR_OF_A_KIND
-                # full house
-                elif 3 in card_vals:
-                    self.quality += FULL_HOUSE
-                    self.hand_type = TYPE_FULL_HOUSE
-                    return self
-            # 3 of a kind. can't be a full house because previous check
-            if 3 in card_vals:
-                self.quality += THREE_OF_A_KIND
-                self.hand_type = TYPE_THREE_OF_A_KIND
-            #  two pair
-            elif list(card_vals).count(2) == 2:
-                self.quality += TWO_PAIR
-                self.hand_type = TYPE_TWO_PAIR
-            # one pair
-            elif 2 in card_vals:
-                self.quality += ONE_PAIR
-                self.hand_type = TYPE_ONE_PAIR
-            return self
-        # card vals sums to 4
-        elif self.joker_count == 1:
-            # implies [3, 1]
-            if 3 in card_vals:
-                self.quality += FOUR_OF_A_KIND
-                self.hand_type = TYPE_FOUR_OF_A_KIND
-            # [2, 1, 1] or [2, 2] possible here
-            elif 2 in card_vals:
-                # implies [2, 2]
-                if len(card_vals) == 2:
-                    self.quality += FULL_HOUSE
-                    self.hand_type = TYPE_FULL_HOUSE
-                # implies [2, 1, 1]
-                else:
-                    self.quality += THREE_OF_A_KIND
-                    self.hand_type = TYPE_THREE_OF_A_KIND
-            # [1,1,1,1] is the only option left
-            else:
-                self.quality += ONE_PAIR
-                self.hand_type = TYPE_ONE_PAIR
-        # card vals sum to 3
-        elif self.joker_count == 2:
-            # implies [2, 1]
-            if 2 in card_vals:
-                self.quality += FOUR_OF_A_KIND
-                self.hand_type = TYPE_FOUR_OF_A_KIND
-            # [1, 1, 1] is the only option left
-            else:
-                self.quality += THREE_OF_A_KIND
-                self.hand_type = TYPE_THREE_OF_A_KIND
-        # card vals sum to 2 and we know its not [2] so it must be [1,1]
-        elif self.joker_count == 3:
-            self.quality += FOUR_OF_A_KIND
-            self.hand_type = TYPE_FOUR_OF_A_KIND
+        card_vals = list(card_counter.values())
+        card_vals.sort(reverse=True)
+
+        # five of a kind
+        if card_vals in [[5], [4], [3], [2], [1], []]:
+            self.hand_type = FIVE_OF_A_KIND
+        # four of a kind
+        elif card_vals in [[4,1], [3,1], [2,1], [1,1]]:
+            self.hand_type = FOUR_OF_A_KIND
+        # full house
+        elif card_vals in [[3,2], [2,2]]:
+            self.hand_type = FULL_HOUSE
+        # thee of a kind
+        elif card_vals in [[3,1,1], [2,1,1], [1,1,1]]:
+            self.hand_type = THREE_OF_A_KIND
+        # two pair
+        elif card_vals in [[2,2,1]]:
+            self.hand_type = TWO_PAIR
+        # pair
+        elif card_vals in [[2,1,1,1], [1,1,1,1]]:
+            self.hand_type = ONE_PAIR
+
         return self
 
 def part_1(camel_hands: list[str]) -> int:
     parsed_hands: list[Hand] = [
-        Hand(cards=parts[0], bet=int(parts[1]), joker_count=parts[0].count('J')).calculate_quality()
+        Hand(cards=parts[0], bet=int(parts[1])).calculate_quality().calculate_tie_break()
         for hand in camel_hands if (parts := hand.split(" "))
     ]
 
@@ -168,7 +72,7 @@ def part_1(camel_hands: list[str]) -> int:
 
 def part_2(camel_hands: list[str]) -> int:
     parsed_hands: list[Hand] = [
-        Hand(cards=parts[0], bet=int(parts[1]), joker_count=parts[0].count('J')).calculate_quality_jokers()
+        Hand(cards=parts[0], bet=int(parts[1]), jacks_are_wild=True).calculate_quality().calculate_tie_break()
         for hand in camel_hands if (parts := hand.split(" "))
     ]
 
--- day7/python/test.py
+++ day7/python/test.py
@@ -9,198 +9,125 @@ def test_part_1(camel_hands):
 
 def test_parse_hand():
     # high card
-    hand = Hand(cards='2684T', bet=825, joker_count=0)
+    hand = Hand(cards='2684T', bet=825)
     hand.calculate_quality()
-    #          2               6               8               4               T
-    expected = 1 * (10 ** 8) + 5 * (10 ** 6) + 7 * (10 ** 4) + 3 * (10 ** 2) + 9 * (10 ** 0)
+    expected = 0
+    assert expected == hand.hand_type
 
     # one pair
-    hand = Hand(cards='6685T', bet=825, joker_count=0)
+    hand = Hand(cards='6685T', bet=825)
     hand.calculate_quality()
 
-    #          6               6               8               5               T
-    expected = 5 * (10 ** 8) + 5 * (10 ** 6) + 7 * (10 ** 4) + 4 * (10 ** 2) + 9 * (10 ** 0) + 10**10
-    assert expected == hand.quality
+    expected = 1
+    assert expected == hand.hand_type
 
     # two pair
-    hand = Hand(cards='55AJJ', bet=825, joker_count=0)
+    hand = Hand(cards='55AJJ', bet=825)
     hand.calculate_quality()
 
-    #          5               5               A                J                J
-    expected = 4 * (10 ** 8) + 4 * (10 ** 6) + 13 * (10 ** 4) + 10 * (10 ** 2) + 10 * (10 ** 0) + 10**11
-    assert expected == hand.quality
+    expected = 2
+    assert expected == hand.hand_type
 
     # three of a kind
-    hand = Hand(cards='77A7J', bet=825, joker_count=0)
+    hand = Hand(cards='77A7J', bet=825)
     hand.calculate_quality()
 
-    #          7               7               A                7               J
-    expected = 6 * (10 ** 8) + 6 * (10 ** 6) + 13 * (10 ** 4) + 6 * (10 ** 2) + 10 * (10 ** 0) + 10**12
-    assert expected == hand.quality
+    expected = 3
+    assert expected == hand.hand_type
 
     # full house
-    hand = Hand(cards='55KKK', bet=825, joker_count=0)
+    hand = Hand(cards='55KKK', bet=825)
     hand.calculate_quality()
 
-    #          5               5               K                K                K
-    expected = 4 * (10 ** 8) + 4 * (10 ** 6) + 12 * (10 ** 4) + 12 * (10 ** 2) + 12 * (10 ** 0) + 10**13
-    assert expected == hand.quality
+    expected = 4
+    assert expected == hand.hand_type
 
     # four of a kind
-    hand = Hand(cards='5KKKK', bet=825, joker_count=0)
+    hand = Hand(cards='5KKKK', bet=825)
     hand.calculate_quality()
 
-    #          5               K                K                K                K
-    expected = 4 * (10 ** 8) + 12 * (10 ** 6) + 12 * (10 ** 4) + 12 * (10 ** 2) + 12 * (10 ** 0) + 10**14
-    assert expected == hand.quality
+    expected = 5
+    assert expected == hand.hand_type
 
     # five of a kind
-    hand = Hand(cards='KKKKK', bet=825, joker_count=0)
+    hand = Hand(cards='KKKKK', bet=825)
     hand.calculate_quality()
 
-    #          K                K                K                K                K
-    expected = 12 * (10 ** 8) + 12 * (10 ** 6) + 12 * (10 ** 4) + 12 * (10 ** 2) + 12 * (10 ** 0) + 10**15
-    assert expected == hand.quality
+    expected = 6
+    assert expected == hand.hand_type
 
 def test_parse_hand_joker():
-    # zero jokers
-    # high card
-    hand = Hand(cards='2684T', bet=825, joker_count=0)
-    hand.calculate_quality_jokers()
-    #          2               6               8               4               T
-    expected = 2 * (10 ** 8) + 6 * (10 ** 6) + 8 * (10 ** 4) + 4 * (10 ** 2) + 10 * (10 ** 0)
-
-    # one pair
-    hand = Hand(cards='6685T', bet=825, joker_count=0)
-    hand.calculate_quality_jokers()
-
-    #          6               6               8               5               T
-    expected = 6 * (10 ** 8) + 6 * (10 ** 6) + 8 * (10 ** 4) + 5 * (10 ** 2) + 10 * (10 ** 0) + 10**10
-    assert expected == hand.quality
-
-    # two pair
-    hand = Hand(cards='55AQQ', bet=825, joker_count=0)
-    hand.calculate_quality_jokers()
-
-    #          5               5               A                Q                Q
-    expected = 5 * (10 ** 8) + 5 * (10 ** 6) + 13 * (10 ** 4) + 11 * (10 ** 2) + 11 * (10 ** 0) + 10**11
-    assert expected == hand.quality
-
-    # three of a kind
-    hand = Hand(cards='77A7Q', bet=825, joker_count=0)
-    hand.calculate_quality_jokers()
-
-    #          7               7               A                7               Q
-    expected = 7 * (10 ** 8) + 7 * (10 ** 6) + 13 * (10 ** 4) + 7 * (10 ** 2) + 11 * (10 ** 0) + 10**12
-    assert expected == hand.quality
-
-    # full house
-    hand = Hand(cards='55KKK', bet=825, joker_count=0)
-    hand.calculate_quality_jokers()
-
-    #          5               5               K                K                K
-    expected = 5 * (10 ** 8) + 5 * (10 ** 6) + 12 * (10 ** 4) + 12 * (10 ** 2) + 12 * (10 ** 0) + 10**13
-    assert expected == hand.quality
-
-    # four of a kind
-    hand = Hand(cards='5KKKK', bet=825, joker_count=0)
-    hand.calculate_quality_jokers()
-
-    #          5               K                K                K                K
-    expected = 5 * (10 ** 8) + 12 * (10 ** 6) + 12 * (10 ** 4) + 12 * (10 ** 2) + 12 * (10 ** 0) + 10**14
-    assert expected == hand.quality
-
-    # five of a kind
-    hand = Hand(cards='KKKKK', bet=825, joker_count=0)
-    hand.calculate_quality_jokers()
-
-    #          K                K                K                K                K
-    expected = 12 * (10 ** 8) + 12 * (10 ** 6) + 12 * (10 ** 4) + 12 * (10 ** 2) + 12 * (10 ** 0) + 10**15
-    assert expected == hand.quality
-
     # one joker
-    hand = Hand(cards='2QJ69', bet=825, joker_count=1)
-    hand.calculate_quality_jokers()
-    #          2               Q                J               6               9                two of a kind   
-    expected = 2 * (10 ** 8) + 11 * (10 ** 6) + 1 * (10 ** 4) + 6 * (10 ** 2) + 9 * (10 ** 0) + 10 ** 10
-    assert expected == hand.quality
+    hand = Hand(cards='2QJ69', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 1
+    assert expected == hand.hand_type
 
-    hand = Hand(cards='QQJ69', bet=825, joker_count=1)
-    hand.calculate_quality_jokers()
-    #          Q                Q                J               6               9               three of a kind   
-    expected = 11 * (10 ** 8) + 11 * (10 ** 6) + 1 * (10 ** 4) + 6 * (10 ** 2) + 9 * (10 ** 0) + 10 ** 12
-    assert expected == hand.quality
+    hand = Hand(cards='QQJ69', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 3
+    assert expected == hand.hand_type
 
-    hand = Hand(cards='QQJ66', bet=825, joker_count=1)
-    hand.calculate_quality_jokers()
-    #          Q                Q                J               6               6               full house   
-    expected = 11 * (10 ** 8) + 11 * (10 ** 6) + 1 * (10 ** 4) + 6 * (10 ** 2) + 6 * (10 ** 0) + 10 ** 13
-    assert expected == hand.quality
+    hand = Hand(cards='QQJ66', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 4
+    assert expected == hand.hand_type
 
-    hand = Hand(cards='QQJQ6', bet=825, joker_count=1)
-    hand.calculate_quality_jokers()
-    #          Q                Q                J               Q                6               four of a kind   
-    expected = 11 * (10 ** 8) + 11 * (10 ** 6) + 1 * (10 ** 4) + 11 * (10 ** 2) + 6 * (10 ** 0) + 10 ** 14
-    assert expected == hand.quality
+    hand = Hand(cards='QQJQ6', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 5
+    assert expected == hand.hand_type
 
-    hand = Hand(cards='QQJQQ', bet=825, joker_count=1)
-    hand.calculate_quality_jokers()
-    #          Q                Q                J               Q                Q                five of a kind   
-    expected = 11 * (10 ** 8) + 11 * (10 ** 6) + 1 * (10 ** 4) + 11 * (10 ** 2) + 11 * (10 ** 0) + 10 ** 15
-    assert expected == hand.quality
+    hand = Hand(cards='QQJQQ', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 6
+    assert expected == hand.hand_type
 
     # two jokers
-    hand = Hand(cards='JQJ67', bet=825, joker_count=2)
-    hand.calculate_quality_jokers()
-    #          J               Q                J               6               7               three of a kind   
-    expected = 1 * (10 ** 8) + 11 * (10 ** 6) + 1 * (10 ** 4) + 6 * (10 ** 2) + 7 * (10 ** 0) + 10 ** 12
-    assert expected == hand.quality
+    hand = Hand(cards='JQJ67', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 3
+    assert expected == hand.hand_type
 
-    hand = Hand(cards='JQJ66', bet=825, joker_count=2)
-    hand.calculate_quality_jokers()
-    #          J               Q                J               6               6               four of a kind   
-    expected = 1 * (10 ** 8) + 11 * (10 ** 6) + 1 * (10 ** 4) + 6 * (10 ** 2) + 6 * (10 ** 0) + 10 ** 14
-    assert expected == hand.quality
+    hand = Hand(cards='JQJ66', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 5
+    assert expected == hand.hand_type
 
-    hand = Hand(cards='J6J66', bet=825, joker_count=2)
-    hand.calculate_quality_jokers()
-    #          J               6               J               6               6               five of a kind   
-    expected = 1 * (10 ** 8) + 6 * (10 ** 6) + 1 * (10 ** 4) + 6 * (10 ** 2) + 6 * (10 ** 0) + 10 ** 15
-    assert expected == hand.quality
+    hand = Hand(cards='J6J66', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 6
+    assert expected == hand.hand_type
 
     # three jokers
-    hand = Hand(cards='J4J2J', bet=825, joker_count=3)
-    hand.calculate_quality_jokers()
-    #          J               4               J               2               J               four of a kind   
-    expected = 1 * (10 ** 8) + 4 * (10 ** 6) + 1 * (10 ** 4) + 2 * (10 ** 2) + 1 * (10 ** 0) + 10 ** 14
-    assert expected == hand.quality
+    hand = Hand(cards='J4J2J', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 5
+    assert expected == hand.hand_type
 
-    hand = Hand(cards='J8J8J', bet=825, joker_count=3)
-    hand.calculate_quality_jokers()
-    #          J               8               J               8               J               five of a kind   
-    expected = 1 * (10 ** 8) + 8 * (10 ** 6) + 1 * (10 ** 4) + 8 * (10 ** 2) + 1 * (10 ** 0) + 10 ** 15
-    assert expected == hand.quality
+    hand = Hand(cards='J8J8J', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 6
+    assert expected == hand.hand_type
 
     # 4 jokers
-    hand = Hand(cards='J3JJJ', bet=825, joker_count=4)
-    hand.calculate_quality_jokers()
-    #          J               3               J               J               J               five of a kind   
-    expected = 1 * (10 ** 8) + 3 * (10 ** 6) + 1 * (10 ** 4) + 1 * (10 ** 2) + 1 * (10 ** 0) + 10 ** 15
-    assert expected == hand.quality
+    hand = Hand(cards='J3JJJ', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 6
+    assert expected == hand.hand_type
 
     # all jokers
-    hand = Hand(cards='JJJJJ', bet=825, joker_count=1)
-    hand.calculate_quality_jokers()
-    #          J               J               J               J               J               five of a kind   
-    expected = 1 * (10 ** 8) + 1 * (10 ** 6) + 1 * (10 ** 4) + 1 * (10 ** 2) + 1 * (10 ** 0) + 10 ** 15
-    assert expected == hand.quality
+    hand = Hand(cards='JJJJJ', bet=825, jacks_are_wild=True)
+    hand.calculate_quality()
+    expected = 6
+    assert expected == hand.hand_type
 
 def test_part_2(camel_hands):
     expected = 5905
     actual = part_2(camel_hands)
     assert expected == actual
 
-test_part_1(camel_hands)
-test_parse_hand_joker()
 test_parse_hand()
+test_parse_hand_joker()
+test_part_1(camel_hands)
 test_part_2(camel_hands)
